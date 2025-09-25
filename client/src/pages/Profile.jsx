7f51aa2a1e86d67211c848e6f1076f1cc99a5f21
atulya1045 <atulyasawant15@gmail.com>
2025-09-25 12:01:04 +0530

    Fix line endings with .gitattributes

--- client/src/pages/Profile.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Profile = () => {
-  return <div> Bhen ke Takke </div>;
-};
-
-export default Profile;
--- client/src/pages/pages.js
+++
@@ -1,8 +0,0 @@
-import Home from "./Home";
-import Profile from "./Profile"
-import Shop from "./Shop"
-import Login from "./Login";
-import Signup from "./Signup";
-import Paint from "./Paint";
-
-export { Home, Login, Signup, Profile, Shop, Paint };
